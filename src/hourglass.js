a41983c524ab4405c6fb1e2a8b9b613becf8e24b
mp40 <philps.matthew@gmail.com>
2019-01-01 13:40:01 +0900

    wrote second failing test and fixed off by one error

--- src/hourglass.js
+++ src/hourglass.js
@@ -5,7 +5,23 @@ Notes: There are 16 hourglasses in the 2D array
 */
 
 const hourglassSum = function (arr) {
+    let result = undefined;
 
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            let temp = arr[i][j] + arr[i][j + 1] + arr[i][j + 2];
+            temp = temp + arr[i + 1][j + 1];
+            temp = temp + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+            if (result === undefined) {
+                result = temp
+            } else {
+                if (result < temp) {
+                    result = temp
+                }
+            }
+        }
+    }
+    return result
 }
 
 module.exports = {
--- src/hourglass.spec.js
+++ src/hourglass.spec.js
@@ -14,4 +14,15 @@ describe.only('find the biggest sum of hourglass in 6x6 2D array', () => {
         ]
         expect(hourglassSum(firstArray)).toEqual(7)
     })
+    it('should return 19 on second test array', () => {
+        const secondArray = [
+            [1, 1, 1, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0],
+            [1, 1, 1, 0, 0, 0],
+            [0, 0, 2, 4, 4, 0],
+            [0, 0, 0, 2, 0, 0],
+            [0, 0, 1, 2, 4, 0]
+        ]
+        expect(hourglassSum(secondArray)).toEqual(19)
+    })
 })
