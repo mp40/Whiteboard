eb4e68414ee79a8f067c2ad80b45e56b66a0f287
Matthew Philps <philps.matthew@gmail.com>
2019-01-06 17:40:30 +0900

    test passed, added extra tests and passed

--- src/twoSum.js
+++ src/twoSum.js
@@ -1,5 +1,16 @@
 const twoSum = function (arr, target){
-    return
+    let result = [];
+    let count = 1;
+    arr.forEach((val, dex)=>{
+        for(let i = count; i< arr.length; i++){
+            if (result.length === 2){return}
+            if(val + arr[i]===target){
+                result.push(dex,i)
+            }
+        }
+    })
+
+   return result
 }
 
-module.exports = twoSum
+module.exports = {twoSum}
--- src/twoSum.spec.js
+++ src/twoSum.spec.js
@@ -1,7 +1,10 @@
-const twoSum = require('./twoSum')
+const {twoSum} = require('./twoSum')
 
 describe("twoSum function",()=>{
+    const testArray = [2,7,11,15]
     it('should return an array with two index references which add up to the target',()=>{
-        expect(twoSum([2,7,11,15], 9)).toEqual([0,1])
+        expect(twoSum(testArray, 9)).toEqual([0,1])
+        expect(twoSum(testArray, 18)).toEqual([1,2])
+        expect(twoSum(testArray, 17)).toEqual([0,3])
     })
 })
