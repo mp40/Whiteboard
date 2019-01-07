10bc7e988e3fe2dfdca97dbe8025bf0aba618adf
Matthew Philps <philps.matthew@gmail.com>
2019-01-07 15:56:44 +0900

    wrote edge case test for missingValue

--- src/missingValue.js
+++ src/missingValue.js
@@ -1,5 +1,16 @@
 function solution(A) {
     // write your code in JavaScript (Node.js 8.9.4)
+    if (A.length<1){
+        return 1
+    }
+    const sorted = A.sort(function(a,b){
+        return a-b
+    })
+    for (let i=0; i<sorted.length; i++){
+        if(A.indexOf(i+1)=== -1){
+            return i+1
+        }
+    }
 }
 
 module.exports = solution
--- src/missingValue.spec.js
+++ src/missingValue.spec.js
@@ -4,4 +4,10 @@ describe("Find missing value in array of intergers",()=>{
     it('should return the missing element',()=>{
         expect(solution([2,3,1,5])).toEqual(4)
     })
+    it('should return 1 if 1 missing',()=>{
+        expect(solution([2,3,4])).toEqual(1)
+    })
+    it('should return 1 if array is empty',()=>{
+        expect(solution([])).toEqual(1)
+    })
 })
