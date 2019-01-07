a4459b495d5ba7658e6bb2199950bb5ed2cbe183
Matthew Philps <philps.matthew@gmail.com>
2019-01-07 16:09:29 +0900

    passed edge cases for missingValue

--- src/missingValue.js
+++ src/missingValue.js
@@ -6,6 +6,13 @@ function solution(A) {
     const sorted = A.sort(function(a,b){
         return a-b
     })
+    if(sorted[0] !== 1){
+        return 1
+    }
+    if(sorted[sorted.length-1] === sorted.length){
+        return sorted.length+1
+    }
+
     for (let i=0; i<sorted.length; i++){
         if(A.indexOf(i+1)=== -1){
             return i+1
