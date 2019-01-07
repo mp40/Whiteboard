c85f325268502104fb2c2326c2943d2b7f0fe199
Matthew Philps <philps.matthew@gmail.com>
2019-01-07 15:25:03 +0900

    passed test for frog jump

--- src/frogJump.spec.js
+++ src/frogJump.spec.js
@@ -4,4 +4,7 @@ describe('Frog Jump Problem',()=>{
     it('should return minimum number of jumps',()=>{
         expect(solution(10,85,30)).toEqual(3)
     })
+    it('should return 0 if X is same as Y',()=>{
+        expect(solution(10,10,30)).toEqual(0)
+    })
 })
