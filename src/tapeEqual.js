8bf4a628c5dfeeaf9d13e85fbd5f812759f050fe
Matthew Philps <philps.matthew@gmail.com>
2019-01-07 20:32:52 +0900

    tests added and passed for tapeEqual

--- src/tapeEqual.js
+++ src/tapeEqual.js
@@ -3,12 +3,31 @@ function split(num,len){
 }
 
 function calculateDiff(tape, total){
-    return (total-tape)-tape
+    let result = (total-tape)-tape
+    return result < 0 ? result*-1 : result
+}
+
+function findResult(temp, result){
+    return result === undefined ? temp : temp < result ? temp : result
 }
 
 function solution(A) {
     // write your code in JavaScript (Node.js 8.9.4)
+    let result = undefined;
+    let total = A.reduce((sum,num)=>{
+        return sum + num;
+    });
+    let sum = 0;
 
+    for(let i=0; i<A.length-1; i++){
+        sum += A[i];
+        if (split(A[i],A.length)){
+            result = findResult(calculateDiff(sum,total),result) 
+            // let temp = calculateDiff(sum,total)
+            // result === undefined || result > temp ? result = temp : result = result 
+        }
+    }
+    return result
 }
 
-module.exports = {solution, split, calculateDiff};
+module.exports = {solution, split, calculateDiff, findResult};
--- src/tapeEqual.spec.js
+++ src/tapeEqual.spec.js
@@ -1,4 +1,4 @@
-const {solution, split, calculateDiff} = require('./tapeEqual')
+const {solution, split, calculateDiff, findResult} = require('./tapeEqual')
 
 describe('tape equilibrium problem',()=>{
     const testArray = [3,1,2,4,3]
@@ -13,7 +13,15 @@ describe('tape equilibrium problem',()=>{
         expect(calculateDiff(4, 13)).toEqual(5)
         expect(calculateDiff(6, 13)).toEqual(1)
     })
-    // it('should return smallest difference it cuts',()=>{
-    //     expect(solution([testArray])).toEqual(1)
-    // })
+    it('should return positive numbers with helper calculateDiff',()=>{
+        expect(calculateDiff(10,13)).toEqual(7)
+    })
+    it('should find smallest of two values',()=>{
+        expect(findResult(7, undefined)).toEqual(7);
+        expect(findResult(5, 7)).toEqual(5);
+        expect(findResult(2,1)).toEqual(1)
+    })
+    it('should return smallest difference it cuts',()=>{
+        expect(solution(testArray)).toEqual(1)
+    })
 })
