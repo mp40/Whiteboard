f2d504230f6a64ed7b81ec39dfcf3bcad3ed7505
Matthew Philps <philps.matthew@gmail.com>
2019-01-07 13:01:29 +0900

    refactor of binaryGap

--- src/binaryGap.js
+++ src/binaryGap.js
@@ -4,25 +4,17 @@ function convertToBinary(num){
 
 function solution(N) {
     // write your code in JavaScript (Node.js 8.9.4)
-    const binary = N.toString(2)
-    console.log(binary)
-    let count = false
+    const binary = convertToBinary(N)
     let result = 0;
     let temp = 0;
     
     for(let i=0; i<binary.length; i++){
-        if (i === "1"){
-            if (count === false){
-                temp = 0            
-            }
-            count = !count
+        if (binary.charAt(i) === "0"){
+            temp++
+        } else {
+            result = temp > result ? temp : result;
+            temp = 0
         }
-     if (i==="0" && count){
-         temp++
-         if (temp > result){
-             result = temp
-         }
-     }   
     }
     return result
 }
