ed6147401cdd8dc04c0eb77132f24db154ab96bc
Matthew Philps <philps.matthew@gmail.com>
2019-01-08 16:11:00 +0900

    backets problem TDD'd and working

--- src/brackets.js
+++ src/brackets.js
@@ -1,10 +1,40 @@
+function checkOpener(S, i){
+const opener = "({[";
+const char = S.charAt(i);
+
+return opener.includes(char) ? true : false
+
+}
+
 function solution(S) {
     // write your code in JavaScript (Node.js 8.9.4)
+    
     if(S.length%2 !== 0){
         return 0
     }
     if(S.length === 0){
         return 1
     }
+    const opener = "({["
+    const closer = {
+        "(": ")",
+        '{':"}",
+        '[':']'
+    }
+    const stopper = "!"
+    S = S+stopper
+    let stack = [stopper]
+    for (let i = 0; i<S.length; i++){
+        if(checkOpener(S,i)){
+            let temp = S.charAt(i);
+            stack.push(closer[temp])
+        } else {
+            if(stack.pop() !== S.charAt(i)){
+                return 0
+            }
+        }        
+    }
+
+    return 1
 }
-module.exports = solution
+module.exports = {solution, checkOpener}
--- src/brackets.spec.js
+++ src/brackets.spec.js
@@ -1,4 +1,4 @@
-const solution = require('./brackets')
+const {solution, checkOpener} = require('./brackets')
 
 describe.only('finding if string has nested brackets',()=>{
     it('should return 0 if input string is an odd length',()=>{
@@ -6,5 +6,18 @@ describe.only('finding if string has nested brackets',()=>{
     })
     it('should return 1 if input string is empty',()=>{
         expect(solution("")).toEqual(1)
+    })
+    it('should use helper function to check if opener string includes tested val',()=>{
+        expect(checkOpener("()", 0)).toEqual(true)
+        expect(checkOpener(")(", 0)).toEqual(false)
+    })
+    it('should return 1 if matching brackets are parameter',()=>{
+        expect(solution("()")).toEqual(1)
+    })
+    it('should return 1 if given multiple nested brackets',()=>{
+        expect(solution("{[()()]}")).toEqual(1)
+    })
+    it('should reurn 0 if given multiple unnested brackets',()=>{
+        expect(solution("([)()]")).toEqual(0)
     })
 })
