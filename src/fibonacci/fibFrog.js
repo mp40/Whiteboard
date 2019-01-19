cf82c1c496e2a2e250a1588ebcdcb779fa499c8a
Matthew Philps <philps.matthew@gmail.com>
2019-01-19 21:46:07 +0900

    started fibFrog

---
+++ src/fibonacci/fibFrog.js
@@ -0,0 +1,52 @@
+/*
+A small frog wants to get to the other side of a river. 
+The frog is initially located at one bank of the river (position −1) 
+and wants to get to the other bank (position N). 
+The frog can jump over any distance F(K), 
+where F(K) is the K-th Fibonacci number. 
+Luckily, there are many leaves on the river, 
+and the frog can jump between the leaves, 
+but only in the direction of the bank at position N.
+
+The leaves on the river are represented in an array consisting of N integers.
+Consecutive elements of array represent 
+consecutive positions from 0 to N − 1 on the river. 
+Array contains only 0s and/or 1s:
+
+0 represents a position without a leaf;
+1 represents a position containing a leaf.
+
+The goal is to count the minimum number of jumps 
+in which the frog can get to the other side of the river 
+(from position −1 to position N). 
+The frog can jump between positions −1 and N (the banks of the river) 
+and every position containing a leaf.
+*/
+
+const findJumps = function(len){
+    let jumps = [1]
+    // populate array containing possible jump lengths
+    if (len > 1){
+        let last = 1
+        let beforeLast = 0
+        while(last < len){
+            let temp = last + beforeLast
+            beforeLast = last
+            last = temp
+            if (last < len){
+                jumps.push(last)
+            }
+        }
+    }
+    return jumps
+}
+
+const fibFrog = function(arr){
+    
+    let jumps = findJumps(arr.length)
+    console.log(jumps)
+    console.log(arr)
+    
+}
+
+module.exports = {fibFrog, findJumps}
---
+++ src/fibonacci/fibFrog.spec.js
@@ -0,0 +1,13 @@
+const {fibFrog, findJumps} = require('./fibFrog')
+
+describe('the frog jumping to the other side of the river',()=>{
+    it('should populate an array fibonacci numbers less than input array length',()=>{
+        const arr = [0,0,0,1,1,0,1,0,0,0,0]
+        const result = [1,1,2,3,5,8]
+        expect(findJumps(arr.length)).toEqual(result) 
+    })
+    it('should return the minimum number of jumps',()=>{
+        const arr = [0,0,0,1,1,0,1,0,0,0,0]
+        expect(fibFrog(arr)).toBe(3)
+    })
+})
