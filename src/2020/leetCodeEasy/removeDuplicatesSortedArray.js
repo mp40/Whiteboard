cb1aa4a2edc3c79794f4feb4ad8d2c3e2c37e3e0
Matthew Philps <philps.matthew@gmail.com>
2020-01-09 22:02:32 +0900

    added boilerplate and tests for removeDuplicates

--- src/2020/leetCodeEasy/removeDuplicatesSortedArray.js
+++ src/2020/leetCodeEasy/removeDuplicatesSortedArray.js
@@ -11,3 +11,10 @@ you must do this by modifying the input array in-place with O(1) extra memory.
 export const removeDuplicates = (nums) => {
     return null;
 }
+
+/*
+Clarification:
+Confused why the returned value is an integer but your answer is an array?
+Note that the input array is passed in by reference,
+which means modification to the input array will be known to the caller as well.
+*/
