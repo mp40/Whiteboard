9c26b119c0d63fa90549fdaece1d74e43391e083
Matthew Philps <philps.matthew@gmail.com>
2020-01-16 12:36:06 +0900

    three sum

--- src/2020/leetCodeMedium/threeSum.js
+++ src/2020/leetCodeMedium/threeSum.js
@@ -18,21 +18,63 @@ A solution set is:
 ]
 */
 
-export const threeSum = function(nums) {
-    const hashSet = {};
-    nums = nums.sort();
+// below works but fails Time Limit
+// export const threeSum = function(nums) {
+//     const hashSet = {};
+//     nums = nums.sort();
 
-    for(let i=0; i<nums.length; i++){
-        const target = 0 - nums[i];
-        let cache = {};
-        for(let j=i+1; j<nums.length; j++){
-            const difference = target - nums[j];
-            if(cache[difference] !== undefined) {
-                hashSet[`${nums[i]}${nums[cache[difference]]}${nums[j]}`] = [nums[i], nums[cache[difference]], nums[j]];
+//     for(let i=0; i<nums.length; i++){
+//         const target = 0 - nums[i];
+//         let cache = {};
+//         for(let j=i+1; j<nums.length; j++){
+//             const difference = target - nums[j];
+//             if(cache[difference] !== undefined) {
+//                 hashSet[`${nums[i]}+${nums[cache[difference]]}+${nums[j]}`] = [nums[i], nums[cache[difference]], nums[j]];
+//             } else {
+//                 cache[nums[j]] = j
+//             }
+//         }
+//     }
+//     return Object.values(hashSet);
+// };
+
+// after quite a few failed attempts to pass leetcode runtime tests
+// copied below from here:
+// https://leetcode.com/problems/3sum/discuss/281302/JavaScript-with-lots-of-explanatory-comments!
+export const threeSum = function(nums) {
+    const result = [];
+    //sort by numerical value lowest to greatest
+    nums = nums.sort((a, b) => a - b);
+
+    for(let i=0; i<nums.length-2; i++){
+        // break out of loop, cause if bigger than 0 - reaching target is impossible
+        if(nums[i] > 0){
+            break;
+        }
+
+        // if i appears again go to next loop iteration
+        if (i > 0 && nums[i] === nums[i - 1]) continue
+
+        let left = i+1
+        let right = nums.length - 1;
+        while(left < right){
+            const total = nums[i] + nums[left] + nums[right];
+            if(total === 0){
+                result.push([nums[i], nums[left], nums[right]]);
+                while(nums[left] === nums[left+1]){
+                    left++
+                }
+                while(nums[right] === nums[right-1]){
+                    right--
+                }
+                left++
+                right--
+            } else if(total < 0) {
+                left++
             } else {
-                cache[nums[j]] = j
+                right--
             }
         }
     }
-    return Object.values(hashSet);
+    return result;
 };
