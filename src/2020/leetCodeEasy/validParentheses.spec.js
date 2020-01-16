8a4f5ee06a519bbb545ebf9e0f4801b9e3ccc193
Matthew Philps <philps.matthew@gmail.com>
2020-01-16 14:53:06 +0900

    valid parentheses tests added

---
+++ src/2020/leetCodeEasy/validParentheses.spec.js
@@ -0,0 +1,35 @@
+import { isValid } from './validParentheses';
+
+describe('Valid Parentheses', () => {
+    it('should return true for empty strings', () => {
+        const input = ""
+        const output = true
+        expect(isValid(input)).toBe(output)
+    })
+    it('should return correct answer for example 1', () => {
+        const input = "()"
+        const output = true
+        expect(isValid(input)).toBe(output)
+    })
+    it('should return correct answer for example 2', () => {
+        const input = "()[]{}"
+        const output = true
+        expect(isValid(input)).toBe(output)
+    })
+    it('should return correct answer for example 3', () => {
+        const input = "(]"
+        const output = false
+        expect(isValid(input)).toBe(output)
+    })
+    it('should return correct answer for example 4', () => {
+        const input = "([)]"
+        const output = true
+        expect(isValid(input)).toBe(output)
+    })
+    it('should return correct answer for example 5', () => {
+        const input = "{[]}"
+        const output = true
+        expect(isValid(input)).toBe(output)
+    })
+})
+
