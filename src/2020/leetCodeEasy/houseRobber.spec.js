60e44c4190bc6f82914b39e8014665f7ce7212d1
Matthew Philps <philps.matthew@gmail.com>
2020-07-15 06:23:01 +0900

    updated test

--- src/2020/leetCodeEasy/houseRobber.spec.js
+++ src/2020/leetCodeEasy/houseRobber.spec.js
@@ -7,8 +7,13 @@ describe('house robber', () => {
         expect(rob(input)).toBe(output);
     })
     it('should pass the second example', () => {
-        const input = [2,7,9,3,1];
+        const input = [2, 7, 9, 3, 1];
         const output = 12;
+        expect(rob(input)).toBe(output);
+    })
+    it('should return 0 for an empty array', () => {
+        const input = [];
+        const output = 0;
         expect(rob(input)).toBe(output);
     })
 })
