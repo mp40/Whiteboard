58e248d1c74f1dda136130d7b6c3548a36b9d6c6
Matthew Philps <philps.matthew@gmail.com>
2020-07-16 19:12:16 +0900

    searchInsertPosition added

---
+++ src/2020/leetCodeEasy/searchInsertPosition.spec.js
@@ -0,0 +1,28 @@
+import { searchInsert } from './searchInsertPosition';
+
+describe('search insert position', () => {
+    it('should return index of target value', () => {
+        const nums = [1, 3, 5, 6];
+        const target = 5;
+        const output = 2;
+        expect(searchInsert(nums, target)).toBe(output);
+    })
+    it('should return index of position of missing target value', () => {
+        const nums = [1, 3, 5, 6];
+        const target = 2;
+        const output = 1;
+        expect(searchInsert(nums, target)).toBe(output);
+    })
+    it('should return index of position of missing target value located at end of array', () => {
+        const nums = [1, 3, 5, 6];
+        const target = 7;
+        const output = 4;
+        expect(searchInsert(nums, target)).toBe(output);
+    })
+    it('should return index of position of missing target value located at beginning of array', () => {
+        const nums = [1, 3, 5, 6];
+        const target = 0;
+        const output = 0;
+        expect(searchInsert(nums, target)).toBe(output);
+    })
+})
