236612b8047e7dd609d1530ed3f9a4c660d179b7
Matthew Philps <philps.matthew@gmail.com>
2020-07-28 21:02:35 +0900

    added tests

--- src/2020/other/rotateImage.spec.js
+++ src/2020/other/rotateImage.spec.js
@@ -1,6 +1,13 @@
 import { rotate } from './rotateImage';
 
 describe('rotate image', () => {
+    it('should rotate 1 x 1 matrix', () => {
+        const input = [[1]];
+        const output = [[1]];
+
+        expect(rotate(input)).toEqual(output);
+    })
+
     it('should rotate 2 x 2 matrix', () => {
         const input = [
             [1,2],
@@ -24,6 +31,23 @@ describe('rotate image', () => {
             [7,4,1],
             [8,5,2],
             [9,6,3]
+        ]
+
+        expect(rotate(input)).toEqual(output);
+    })
+
+    it('should rotate 4 x 4 matrix', () => {
+        const input = [
+            [1,2,3,4],
+            [5,6,7,8],
+            [9,10,11,12],
+            [13,14,15,16]
+        ];
+        const output = [
+            [13,9,5,1],
+            [14,10,6,2],
+            [15,11,7,3],
+            [16,12,8,4]
         ]
 
         expect(rotate(input)).toEqual(output);
