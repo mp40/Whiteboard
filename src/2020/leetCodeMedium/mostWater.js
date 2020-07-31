a8738a3f9c56bc537a56a67a9b6387e73f87c62e
Matthew Philps <philps.matthew@gmail.com>
2020-08-01 08:13:44 +0900

    added container with most water alg

---
+++ src/2020/leetCodeMedium/mostWater.js
@@ -0,0 +1,28 @@
+// Container With Most Water
+// https://leetcode.com/problems/container-with-most-water/
+
+/*
+Given n non-negative integers,
+where each represents a point at coordinate.
+n vertical lines are drawn.
+Find two lines, which together with x-axis forms a container,
+such that the container contains the most water.
+
+Note:
+You may not slant the container and n is at least 2.
+
+Example:
+Input: [1,8,6,2,5,4,8,3,7]
+Output: 49
+
+Explaination:
+8 and 7 form container with most water.
+7 is the shortest height.
+7 is the distance between the two lines
+height (7) * width (7) = 49
+
+*/
+
+export const maxArea = (height) => {
+
+};
