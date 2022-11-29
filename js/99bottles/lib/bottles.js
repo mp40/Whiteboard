a1aa378a1036cd1d03df6b084295ad629f95b54e
mp40 <philps.matthew@gmail.com>
2022-11-29 20:32:22 +0100

    refactor

--- js/99bottles/lib/bottles.js
+++ js/99bottles/lib/bottles.js
@@ -21,13 +21,6 @@ export class Bottles {
             '1 bottle of beer on the wall.\n';
         }
 
-        if(count === 3) {
-            return '3 bottles of beer on the wall, ' +
-            '3 bottles of beer.\n' +
-            'Take one down and pass it around, ' +
-            '2 bottles of beer on the wall.\n';
-        }
-
         return `${count} bottles of beer on the wall, ` +
         `${count} bottles of beer.\n` +
         'Take one down and pass it around, ' +
