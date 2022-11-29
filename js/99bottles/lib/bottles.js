8a81e3e9a9c2b1cb8f6933ad6f575ff4f625e892
mp40 <philps.matthew@gmail.com>
2022-11-29 19:31:34 +0100

    second test passed

--- js/99bottles/lib/bottles.js
+++ js/99bottles/lib/bottles.js
@@ -1,5 +1,12 @@
 export class Bottles {
-    verse() {
+    verse(count) {
+        if(count === 3) {
+            return '3 bottles of beer on the wall, ' +
+            '3 bottles of beer.\n' +
+            'Take one down and pass it around, ' +
+            '2 bottles of beer on the wall.\n';
+        }
+
         return '99 bottles of beer on the wall, ' +
         '99 bottles of beer.\n' +
         'Take one down and pass it around, ' +
