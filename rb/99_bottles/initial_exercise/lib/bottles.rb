df424874fbba1111ecce03583678eb74e82d2066
mp40 <philps.matthew@gmail.com>
2022-12-04 10:30:04 +0100

    refactor

--- rb/99_bottles/initial_exercise/lib/bottles.rb
+++ rb/99_bottles/initial_exercise/lib/bottles.rb
@@ -21,13 +21,6 @@ class Bottles
             "1 bottle of beer on the wall.\n"
         end
   
-        if count == 3
-            return "3 bottles of beer on the wall, " +
-            "3 bottles of beer.\n" +
-            "Take one down and pass it around, " +
-            "2 bottles of beer on the wall.\n"
-        end
-
         return "#{count} bottles of beer on the wall, " +
         "#{count} bottles of beer.\n" +
         "Take one down and pass it around, " +
