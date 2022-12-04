d2a65a9bf1127cfd0c29d8bb7ec1e7328853e855
mp40 <philps.matthew@gmail.com>
2022-12-04 10:13:54 +0100

    0 bottles test passed

--- rb/99_bottles/initial_exercise/lib/bottles.rb
+++ rb/99_bottles/initial_exercise/lib/bottles.rb
@@ -1,5 +1,12 @@
 class Bottles
     def verse(count)
+        if count == 0
+            return "No more bottles of beer on the wall, " +
+            "no more bottles of beer.\n" +
+            "Go to the store and buy some more, " +
+            "99 bottles of beer on the wall.\n"
+        end
+    
         if count == 1
             return "1 bottle of beer on the wall, " +
             "1 bottle of beer.\n" +
