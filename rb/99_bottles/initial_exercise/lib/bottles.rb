3e4eb62bdd2a7208592a81e60fe70bd0d8cb37d3
mp40 <philps.matthew@gmail.com>
2022-12-04 10:12:00 +0100

    verse 1 test passed

--- rb/99_bottles/initial_exercise/lib/bottles.rb
+++ rb/99_bottles/initial_exercise/lib/bottles.rb
@@ -1,11 +1,19 @@
 class Bottles
     def verse(count)
+        if count == 1
+            return "1 bottle of beer on the wall, " +
+            "1 bottle of beer.\n" +
+            "Take it down and pass it around, " +
+            "no more bottles of beer on the wall.\n"
+        end
+
         if count == 2
             return "2 bottles of beer on the wall, " +
             "2 bottles of beer.\n" +
             "Take one down and pass it around, " +
             "1 bottle of beer on the wall.\n"
-        end    
+        end
+  
         if count == 3
             return "3 bottles of beer on the wall, " +
             "3 bottles of beer.\n" +
