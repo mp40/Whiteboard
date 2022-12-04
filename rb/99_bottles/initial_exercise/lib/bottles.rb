80f6de7f850c2ad6b8bf532f8f834ae840be4e3b
mp40 <philps.matthew@gmail.com>
2022-12-04 10:34:04 +0100

    refactor to use switch statement

--- rb/99_bottles/initial_exercise/lib/bottles.rb
+++ rb/99_bottles/initial_exercise/lib/bottles.rb
@@ -1,30 +1,30 @@
 class Bottles
     def verse(count)
-        if count == 0
+        case count
+        when 0
             return "No more bottles of beer on the wall, " +
             "no more bottles of beer.\n" +
             "Go to the store and buy some more, " +
             "99 bottles of beer on the wall.\n"
-        end
     
-        if count == 1
+        when 1
             return "1 bottle of beer on the wall, " +
             "1 bottle of beer.\n" +
             "Take it down and pass it around, " +
             "no more bottles of beer on the wall.\n"
-        end
 
-        if count == 2
+        when 2
             return "2 bottles of beer on the wall, " +
             "2 bottles of beer.\n" +
             "Take one down and pass it around, " +
             "1 bottle of beer on the wall.\n"
-        end
-  
-        return "#{count} bottles of beer on the wall, " +
-        "#{count} bottles of beer.\n" +
-        "Take one down and pass it around, " +
-        "#{count - 1} bottles of beer on the wall.\n"
+
+        else
+            return "#{count} bottles of beer on the wall, " +
+            "#{count} bottles of beer.\n" +
+            "Take one down and pass it around, " +
+            "#{count - 1} bottles of beer on the wall.\n"
+        end    
     end
     
     def verses(first, last)
