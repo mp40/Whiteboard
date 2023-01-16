b445369fa404f9f473dae46001a08e83a5bec81b
Matthew Philps <30450957+mp40@users.noreply.github.com>
2023-01-16 07:41:38 +0100

    add test cases to palindrome number

--- js/leetCodeEasy/palindromeNumber/index.spec.js
+++ js/leetCodeEasy/palindromeNumber/index.spec.js
@@ -7,11 +7,31 @@ describe("determine if interger is palindrome", () => {
     const output = true;
     expect(isPalindrome(input)).toBe(output);
   });
+
   it("should return false if not valid palindrome", () => {
+    const input = 123;
+    const output = false;
+    expect(isPalindrome(input)).toBe(output);
+  });
+
+  it("should return true for zero", () => {
+    const input = 0;
+    const output = true;
+    expect(isPalindrome(input)).toBe(output);
+  });
+
+  it("should return true for single digit numbers", () => {
+    const input = 1;
+    const output = true;
+    expect(isPalindrome(input)).toBe(output);
+  });
+
+  it("should return false if ends in zero", () => {
     const input = 10;
     const output = false;
     expect(isPalindrome(input)).toBe(output);
   });
+
   it("should return false if negative number", () => {
     const input = -121;
     const output = false;
