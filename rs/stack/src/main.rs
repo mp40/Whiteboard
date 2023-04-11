453620a9c99abd7d34b13b97f13f6f34de71852b
mp40 <philps.matthew@gmail.com>
2023-04-11 21:16:07 +0200

    is empty equals true passed

--- rs/stack/src/main.rs
+++ rs/stack/src/main.rs
@@ -2,6 +2,12 @@ struct Stack {
     capacity: u8
 }
 
+impl Stack {
+    fn is_empty(&self) -> bool {
+        return true;
+    }
+}
+
 fn main() -> Stack {
    let stack = Stack {
     capacity: 3
@@ -14,4 +20,10 @@ fn main() -> Stack {
 fn test_capacity() {
     let stack = main();
     assert_eq!(stack.capacity, 3);
+}
+
+#[test]
+fn test_is_empty() {
+    let stack = main();
+    assert_eq!(stack.is_empty(), true);
 }
